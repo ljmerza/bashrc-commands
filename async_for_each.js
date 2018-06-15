20302f00761b6f5cd032c1799f342540db3eec30
Leonardo Merza <ljmerza@gmail.com>
2018-06-15 08:23:49 -0400

    automated commit 15/06/2018 08:23:49

--- async_for_each.js
+++ async_for_each.js
@@ -1,4 +1,4 @@
-async function async_for_each(array, callback) {
+async function asyncForEach(array, callback) {
 	for (let index = 0; index < array.length; index++) {
 		await callback(array[index], index, array)
 	}
@@ -6,7 +6,7 @@ async function async_for_each(array, callback) {
 
 
 // use
-await sync_helpers.async_for_each(log_files, async log_file => {
+await sync_helpers.asyncForEach(log_files, async log_file => {
 	try {
 		await _sync_a_log(log_file);
 	} catch(err){
